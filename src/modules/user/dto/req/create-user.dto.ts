769ea2b65bc3f3f287996cdc08e7e3ae5086c939
Malyshev Oleksndr <malyshevalex1994@gmail.com>
2024-08-22 15:41:15 +0300

    added some responses, validation and transform

--- src/modules/user/dto/req/create-user.dto.ts
+++ src/modules/user/dto/req/create-user.dto.ts
@@ -1,18 +1,59 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
+import {
+  IsEmail,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Length,
+  Matches,
+  Max,
+  Min,
+} from 'class-validator';
+
+import { TransformHelper } from '../../../../common/transform-helper/transform-helper';
+import { regexConstants } from '../../../../constants/regex-constants';
 
 export class CreateUserDto {
   @ApiProperty()
-  name: string;
+  @IsString()
+  @Transform(TransformHelper.trim)
+  @Transform(TransformHelper.toLowerCase)
+  @Length(2, 20)
+  public readonly name: string;
 
   @ApiProperty()
-  email: string;
+  @IsString()
+  @IsEmail()
+  @Transform(TransformHelper.trim)
+  @Matches(regexConstants.EMAIL)
+  public readonly email: string;
 
   @ApiProperty()
-  password: string;
+  @IsString()
+  @Transform(TransformHelper.trim)
+  @Matches(regexConstants.PASSWORD, {
+    message:
+      'at least one letter (either uppercase or lowercase),' +
+      'at least one digit, at least one special character from the set @$!%_*#?&,' +
+      'at least 8 characters long',
+  })
+  public readonly password: string;
 
   @ApiProperty()
-  age: number;
+  @IsNumber()
+  @Type(() => Number)
+  @IsInt()
+  @Min(16)
+  @Max(60)
+  public readonly age: number;
 
+  @ApiProperty()
+  @IsString()
+  @Transform(TransformHelper.trim)
+  @Matches(regexConstants.PHONE)
   @ApiProperty({ required: false })
-  phone?: string;
+  @IsOptional()
+  public readonly phone?: string;
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,7 +7,16 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiConflictResponse,
+  ApiForbiddenResponse,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
 import { CreateUserDto } from './dto/req/create-user.dto';
 import { UpdateUserDto } from './dto/req/update-user.dto';
@@ -20,11 +29,15 @@ import { UserService } from './user.service';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @ApiBadRequestResponse({ description: 'Bad request' })
+  @ApiConflictResponse({ description: 'Conflict' })
   @Post()
   async create(@Body() dto: CreateUserDto): Promise<ResPrivateUserDto> {
     return await this.userService.create(dto);
   }
 
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
   @ApiBearerAuth()
   @Get('me')
   async getMe(): Promise<ResPublicUserDto> {
@@ -32,18 +45,27 @@ export class UserController {
   }
 
   @ApiBearerAuth()
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
+  @ApiConflictResponse({ description: 'Conflict' })
   @Patch('me')
   async updateMe(@Param('id') id: string, @Body() dto: UpdateUserDto) {
     return await this.userService.update(+id, dto);
   }
 
   @ApiBearerAuth()
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @ApiForbiddenResponse({ description: 'Forbidden' })
   @Delete('me')
   async removeMe(@Param('id') id: string) {
     return await this.userService.remove(+id);
   }
 
   @Get(':id')
+  @ApiNotFoundResponse({ description: 'Not found' })
+  @ApiNoContentResponse({ description: 'User has been removed' })
   public async findOne(@Param('id') id: number): Promise<ResPublicUserDto> {
     return await this.userService.findOne(id);
   }
